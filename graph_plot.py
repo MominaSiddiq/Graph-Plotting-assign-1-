718ee036f5fe72a6fed24608955a5f1672bb8481
MomnaHammad <momnamehar753@gmail.com>
2023-11-10 17:58:45 +0000

    Update graph_plot.py
    setting my code according to PEP-8 rules

--- graph_plot.py
+++ graph_plot.py
@@ -17,14 +17,18 @@ url = "https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Rainfal
 
 xLabel = "Month"
 yLabel = "Rainfall (mm)"
+
 linePlotTitle = 'Monthly Rainfall in UK for year 2019-2023 (Multiple-line Plot)'
 boxPlotTitle = 'Monthly Rainfall Distribution in the UK (Box Plot)'
 histoPlotTitle = 'Distribution of Annual Rainfall in the UK (Histogram)'
+
 figX_Size = 10
 figY_Size = 6
 
+
+
 # defining a function for setting labels and show legends
-def GraphLabelingAndShow(title, myXLabel=xLabel, myYLabel=yLabel):
+def GraphLabelingAndShow(title, myXLabel = xLabel, myYLabel = yLabel):
     """
 
     Parameters
@@ -49,8 +53,9 @@ def GraphLabelingAndShow(title, myXLabel=xLabel, myYLabel=yLabel):
     plt.legend()
     plt.show()
 
+
 def PlotFigure(xSize = figX_Size, ySize = figY_Size):
-    plt.figure(figsize=(xSize, ySize))
+    plt.figure(figsize = (xSize, ySize))
 
 
 
@@ -69,7 +74,7 @@ def plot_line_graph(data):
     Returns
     -------
     Nothing returns
-    this function fetches the data and plot line graph on it.
+    this function taking data and plot line graph on it.
 
     """
 
@@ -81,17 +86,18 @@ def plot_line_graph(data):
         selective_years)]
 
     # plotting multiple line graph for selective years and comparing the rainfall monthly
-    #plt.figure(figsize=(10, 6))
-    
+   
+    # calling the function to plot figure
     PlotFigure()
-    
+
     for Years in selective_years:
         selective_years_data = filtered_years_data[filtered_years_data['year'] == Years]
         plt.plot(selective_years_data.columns[1:13],
                  selective_years_data.values[0][1:13], label=f'year{Years}')
 
-    # calling the function and passing arguments
+    # calling the function for labelling and showing graph
     GraphLabelingAndShow(linePlotTitle)
+
 
 
 # <-------------------------------------------------------------->
@@ -109,21 +115,22 @@ def box_plot(data):
     Returns
     -------
     Nothing returns
-    this function fetches the data and plot box plot on it.
+    this function taking data and plot box plot on it.
 
     """
 
     # Plotting a Box plot for monthly rainfall distribution for different years
-    #plt.figure(figsize=(10, 6))
-
+    
+    # calling the function to plot figure
     PlotFigure()
 
     # making a box plot for each month by dropping all other columns
     data.drop(['year', 'win', 'spr', 'sum',
-                       'aut', 'ann'], axis=1).boxplot()
+                       'aut', 'ann'], axis = 1).boxplot()
 
-    # calling the function and passing arguments
+    # calling the function for labelling and showing graph
     GraphLabelingAndShow(boxPlotTitle)
+
 
 
 # <-------------------------------------------------------------->
@@ -141,36 +148,68 @@ def plot_histogram(data):
     Returns
     -------
     Nothing returns
-    this function fetches the data and plot histogram on it.
+    this function taking data and plot histogram on it.
 
     """
 
     # For annual rainfall distribution plotting Histogram
-    #plt.figure(figsize=(10, 6))
     
+    # calling the function to plot figure
     PlotFigure()
 
     # making a histogram for annual rainfall
-    plt.hist(data['ann'], bins=20,
-             color='lightblue', edgecolor='black')
+    plt.hist(data['ann'], bins = 20,
+             color = 'lightblue', edgecolor = 'black')
 
-    # calling the function and passing arguments
+    # calling the function for labelling and showing graph
     GraphLabelingAndShow(histoPlotTitle, 'Annual Rainfall (mm)', 'Frequency')
 
+
+
 def FetchingDataFromURL():
-    # fetch the data from url and read it into a DataFrame and print it
-    global rainfall_data 
-    rainfall_data = pd.read_csv(url, skiprows=5, delim_whitespace=True)
-    print(rainfall_data)
+    """
+
+    Parameters
+    ----------
+    No parameters
+
+    Returns
+    -------
+    Nothing returns
+    this function fetch the data from url and read it 
+    into a DataFrame and print it
+
+    """
     
+    global rainfall_data
+    rainfall_data = pd.read_csv(url, skiprows = 5, delim_whitespace = True)
+    print(rainfall_data)
+
+
 
 def GraphPlot(data):
+    """
+
+    Parameters
+    ----------
+    data : csv
+        data containing the rainfall data of UK.
+
+    Returns
+    -------
+    Nothing returns
+    this function calling three types of graph plotting functions 
+    and passing data as an argument
+
+    """
+   
     # calling the functions and passing the argument
     plot_line_graph(data)
     box_plot(data)
     plot_histogram(data)
-        
+
+
+
+# calling the data fech and main graph plotting function
 FetchingDataFromURL()
 GraphPlot(rainfall_data)
-    
-    
